b5775caa75285c6dbc32fe6b5b2c80d3fe09ab3e
Shani McDonald <shanilmcdonald@gmail.com>
2019-12-31 20:04:22 -0500

    update

--- pricing-component-with-toggle-master/main.js
+++ pricing-component-with-toggle-master/main.js
@@ -1,23 +1,40 @@
 window.addEventListener('DOMContentLoaded', () => {
+
   const button = document.querySelector('.switch')
   const monthlyPrice = document.querySelector('.monthly')
   const annualPrice = document.querySelector('.annually')
-  console.log(button)
 
-  button.addEventListener('click', e => {
-    // const change = document.querySelector('.switch')
-    const result = document.querySelector('.toggle')
-    // console.log(monthlyPrice.textContent)
-    // result.textContent = annualPrice.textContent
+  let hasBeenToggled = false
 
-    function classToggle() {
-      .classList.add(annualPrice)
-      this.classList.remove(monthlyPrice)
+  button.addEventListener('click', () => {
+    const monthlyBasic = document.querySelector('.basic-monthly')
+    const monthlyPro = document.querySelector('.pro-monthly')
+    const monthlyMaster = document.querySelector('.master-monthly')
+
+    const annualBasic = document.querySelector('.basic-annual')
+    const annualPro = document.querySelector('.pro-annual')
+    const masterAnnual = document.querySelector('.master-annual')
+
+    
+    if (!hasBeenToggled) {
+      button.classList.toggle('toggle-monthly')
+      hasBeenToggled = true
+    } else {
+      button.classList.toggle('toggle-monthly')
+      button.classList.toggle('toggle-annually')
     }
-    classToggle()
-    
-    
-  })
 
+    if (button.classList.contains('toggle-monthly')) {
+      monthlyBasic.textContent = '$19.99'
+      monthlyPro.innerHTML = '$24.99'
+      monthlyMaster.innerHTML = '$39.99'
+    } else if (button.classList.contains('toggle-annually')) {
+      annualPrice.innerHTML = '$199.99'
+      annualPrice.innerHTML = '$249.99'
+      annualPrice.innerHTML = '$399.99'
+    }
+  })
 })
+
+
 
